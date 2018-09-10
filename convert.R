f925d362f39a8f98e2e3d45608472c4349efedfb
ciaran-dk <ciaran@murraynet.dk>
2018-09-10 15:19:27 +0200

    work

---
+++ convert.R
@@ -0,0 +1,91 @@
+
+
+getheader<-function(idx,strlst){
+  str<-strlst[[idx]]
+  if(substr(str,1,16) %in% c("//<DataVariable>","//<MetaVariable>")){
+    vartype<-substr(str,4,15)
+    str<-substr(str,17,nchar(str)-16)
+    str<-strsplit(str,"\" " )
+    str<-data.frame(str,stringsAsFactors=F)
+    names(str)<-c("str")
+    str <- str %>% separate(str,c("param","value"),"=\"") 
+    
+    comment<-str %>% filter(param=="comment")
+    comment<-comment[1,2]
+    if(!is.na(comment)){
+      comment<-strsplit(comment," SDN:")
+      comment<-data.frame(comment,stringsAsFactors=F)
+      names(comment)<-c("comment")
+      comment <- comment %>% separate(comment,c("param","value"),"::",fill = "right") %>%
+        filter(!is.na(value))    
+      str<-str %>% bind_rows(comment)
+    }
+    param<-"Type"
+    value<-vartype
+    df<-data.frame(param,value,stringsAsFactors=F)
+    str<-str %>% 
+      bind_rows(df) %>% 
+      mutate(i=idx)
+  }else{
+    str<-NULL
+  }
+  return(str)
+  
+}
+
+getrows<-function(idx,strlst){
+  str<-strlst[[idx]]
+  if(substr(str,1,2) == "//"){
+    return(idx)
+  }else{
+    return(NA)
+  }
+}
+
+convert<-function(EMODNETfile){ 
+  
+  
+  dat <- readLines(con<-file(EMODNETfile, encoding = "utf-8"))
+  close(con)
+  
+  # Get header info from the data files
+  dflist<-lapply(seq_along(dat),function(x) getheader(x,dat))
+  dflist<-dflist[lapply(dflist, length) > 0]
+  dfhead<-do.call(rbind, dflist) %>% spread(key="param",value="value")
+  
+  dfparams<-dfhead %>% filter(Type=="DataVariable") %>% select(label,P01,P06)
+  
+  # Get the number of lines which are commented out
+  dflist<-sapply(seq_along(dat),function(x) getrows(x,dat))
+  nskip<-max(dflist,na.rm=T)
+  
+  # Get column names from the first line in the data file which is not commented out
+  dfcols <- read.table(EMODNETfile,sep="\t",header=F,stringsAsFactors=F,fileEncoding="UTF-8",
+                       comment.char="",allowEscapes=F,na.strings="NULL",fill=T,skip=nskip) %>% head(1) %>% 
+    gather(key="col")
+  
+  # Get data
+  df <- read.table(EMODNETfile,sep="\t",header=F,stringsAsFactors=F,fileEncoding="UTF-8",
+                   comment.char="",allowEscapes=F,na.strings="NULL",fill=T,skip=nskip+1)
+  
+  # Get column names (V1 ... Vn) for the parameters taken from the headers
+  dfcolvalue <- dfparams %>% inner_join(dfcols,by=c("label"="value"))
+  
+  # Get column names (V1 ... Vn) for the quality code for each parameter (one column to the right of the value column)
+  dfcolqual <- dfcolvalue %>% mutate(col=paste0("V",as.numeric(substr(col,2,9))+1))
+  
+  colmin<-as.numeric(substr(dfcolvalue$col[1],2,99))
+  colmax<-as.numeric(substr(dfcolqual$col[nrow(dfcolqual)],2,99))
+  
+  df <- df %>% gather(key="col",value="value",colmin:colmax)
+  
+  # selecting the first 9 columns of data
+  dfval <- df %>% inner_join(dfcolvalue,by="col") %>% select(V1:V9,value,label,P01,P06,-col)
+  dfqual <- df %>% inner_join(dfcolqual,by="col") %>% rename(quality=value) %>% select(V1:V9,label,quality,P01,P06,-col)
+  
+  df <- dfval %>% 
+    left_join(dfqual)
+  
+  names(df)[1:9]<-dfcols$value[1:9]
+  return(df)
+}
--- main.R
+++ main.R
@@ -1,16 +1,21 @@
 ## processing EMODnet chemistry data
-
 library(tidyverse)
 
+source("convert.R")
+
+EMODNETfile<-"data/Black_Sea_BIOTA.txt" 
+
+df <- convert(EMODNETfile)
+
+
+
+P01file <- "EMDchem_BlkSea_MedSea_P01_20180523.txt"
+
 # load file containing list of P01 components
-P01file<-"data/P01_subcomponents.txt" # UTF-8 (BOM)
+P01file<-"data/P01_subcomponentsx.txt" # UTF-8 (BOM)
 
-#dfP01 <- read.table(P01file,header=T,sep="\t",stringsAsFactors=F,encoding="UTF-8",comment.char="",skip=2650,allowEscapes=F,na.strings = "NULL")
-dfP01 <- read.table(P01file,header=T,sep="\t",stringsAsFactors=F,encoding="UTF-8",comment.char="",allowEscapes=F,na.strings = "NULL")
+dfP01 <- read.table(P01file,header=T,sep="\t",stringsAsFactors=F,fileEncoding="UTF-8",comment.char="",quote="\'" ,allowEscapes=F,na.strings = "NULL",fill=T)
 
 
-"data_from_Mediterranean_Biota_Contaminants.txt" # 1252
-"data_from_Mediterranean_Biota_Contaminants_Time_series.txt" # 1252
-"Black_Sea_BIOTA.txt" # 1252
 
-dfP01[2651,1]
+# 1252
